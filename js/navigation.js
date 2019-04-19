2229439f756ca138a37ce88e5a65dfbcf5cc4ed5
Catherine <catherineep4189@gmail.com>
2019-04-19 12:08:35 +0100

    Update navigation.js
    script for opening and closing navigation via icons

---
+++ js/navigation.js
@@ -0,0 +1,8 @@
+//Javascript for navigation menu overlay - width set to 100% overlay when menu is opened & set to 0% once closed//
+  function openNav() {
+   document.getElementById("GHRLnav") .style.width="100%";
+ }
+
+  function closeNav() {
+   document.getElementById("GHRLnav") .style.width="0%";
+ }
